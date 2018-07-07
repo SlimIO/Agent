70093dfaa8d14a669dad3b0c42df07acad6d6bfb
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2018-07-07 14:05:58 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const Core = require("@slimio/core");
+
+/**
+ * @async
+ * @function main
+ * @returns {Promise<void>}
+ */
+async function main() {
+    const core = new Core(__dirname);
+    await core.initialize();
+
+    // Handle exit signal!
+    process.on("SIGINT", async() => {
+        console.log("(SIGINT Catched) - Exiting SlimIO Agent properly (...please wait)");
+        try {
+            await core.exit();
+            setImmediate(process.exit);
+        }
+        catch (error) {
+            console.error(error);
+            process.exit(1);
+        }
+    });
+}
+main().catch(console.error);
