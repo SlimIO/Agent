316566aca6a7d1704e84749e88f8d7834be653b9
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2018-09-29 11:52:50 +0200

    Update packages and update main handler

--- index.js
+++ index.js
@@ -6,21 +6,20 @@ const Core = require("@slimio/core");
  * @returns {Promise<void>}
  */
 async function main() {
-    const core = new Core(__dirname);
-    await core.initialize();
+    console.time("start_core");
+    const core = await (new Core(__dirname)).initialize();
+    console.timeEnd("start_core");
     console.log("SlimIO Agent started!");
 
     // Handle exit signal!
-    process.on("SIGINT", async() => {
-        console.log("(SIGINT Catched) - Exiting SlimIO Agent properly (...please wait)");
-        try {
-            await core.exit();
+    process.on("SIGINT", () => {
+        console.error("Exiting SlimIO Agent (please wait)");
+        core.exit().then(() => {
             setImmediate(process.exit);
-        }
-        catch (error) {
+        }).catch(function mainErrorHandler(error) {
             console.error(error);
             process.exit(1);
-        }
+        });
     });
 }
 main().catch(console.error);
