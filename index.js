7d28d3a41846d2cf2e8a9e3a6b820f3ba0d5f790
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2018-12-04 14:02:34 +0100

    feat: add argParser

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ require("v8-compile-cache");
 
 process.title = "SlimIO";
 const Core = require("@slimio/core");
+const ArgParser = require("@slimio/arg-parser");
+
+// Retrieve argv
+const argv = new ArgParser("v0.1.0", "SlimIO Agent CLI")
+    .addCommand("--silent", "Enable silent mode")
+    .addCommand("-a --autoreload [number=500]", "Configuration Autoreload delay")
+    .parse();
 
 /**
  * @async
@@ -10,7 +17,10 @@ const Core = require("@slimio/core");
  */
 async function main() {
     console.time("start_core");
-    const core = await (new Core(__dirname, { silent: true })).initialize();
+    const core = await (new Core(__dirname, {
+        silent: argv.get("silent"),
+        autoReload: argv.get("autoreload")
+    })).initialize();
     console.timeEnd("start_core");
     console.log("SlimIO Agent started!");
 
